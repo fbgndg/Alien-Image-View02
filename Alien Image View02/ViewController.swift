7262c0d4404ad1646dece2088cf3e8a8ad97f520
ybh <2616442@naver.com>
2019-03-28 15:38:26 +0900

    사진

--- Alien Image View02/ViewController.swift
+++ Alien Image View02/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var alienImageViewer: UIImageView!
+    @IBOutlet weak var countLabel: UILabel!
+    
+    var count = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        alienImageViewer.image = UIImage(named: "frame1.png")
+        //countLabel.text = String(count)
+        countLabel.text = "frame\(count).png"
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        //print("Button Pressed")
+        count+=1
+        
+        if (count>5){
+            count = 1
+        }
+        
+        alienImageViewer.image = UIImage(named: "frame\(count).png")
+        countLabel.text = "frame\(count).png"
+        
+    
+        
+    }
+    
 }
 
